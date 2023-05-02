bc7c15a300d02ebf7abc29d23d220965ef1b48d4
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-05-02 22:35:29 +0900

    hoge

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,14 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
 
   def index
+    #@item = item.new
     
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :image, :item_description, :price).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   
-  resources :users  only: [:new, :create]
+  #resources :users  only: [:new, :create]
   resources :items, only: [:new, :create]
   
 end
