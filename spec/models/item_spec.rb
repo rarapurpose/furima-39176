bfd7e386746015082d3ab91840880668ce18a572
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-05-04 19:27:51 +0900

    修正２回目

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -131,9 +131,9 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it "価格の範囲は9,999,999以上だと出品できない" do
-        @item.price = 10,000,000
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
+        @item.price = 10_000_000
+        @item.valid?        
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
     end
 
