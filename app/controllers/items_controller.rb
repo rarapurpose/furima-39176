ca3005a7c25fb42cfb00122582a1f67e12cd917a
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-05-08 20:24:29 +0900

    修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,10 +39,11 @@ class ItemsController < ApplicationController
   end  
 
   def destroy
-   if @item.user_id != current_user.id
-    @item.destroy
-    redirect_to root_path   
+   if @item.user_id == current_user.id 
+      @item.destroy      
    end
+   redirect_to root_path   
+    
 end
 
 
