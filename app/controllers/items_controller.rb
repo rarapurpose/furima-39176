55f045e5bbe38ce7948bc9b4bb9dc569d9d2590b
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-05-08 18:01:16 +0900

    review 修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,10 +30,8 @@ class ItemsController < ApplicationController
 
   def update
     #@item = Item.find(params[:id])
-    @item.update(item_params)
-    
-    if @item.valid?
-      redirect_to item_path(item_params)
+    if @item.update(item_params)    
+     redirect_to item_path(item_params)
     else
       render :edit
     end
