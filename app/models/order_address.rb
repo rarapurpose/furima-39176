cb3ad87d99fcb50cd2c29c06c676b2ef6dd39d38
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-05-10 00:36:11 +0900

    error(render)表示

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model 
-  attr_accessor :user_id, :item_id, :postal_code, :shipping_prefecture_id, :city, :street, :apartment, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :shipping_prefecture_id, :city, :street, :apartment, :phone_number, :token
 
   with_options presence: true do  
   validates :user_id
