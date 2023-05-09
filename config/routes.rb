3ec7e82ce09a1f8c6974c85bb76024b0e511f73d
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-05-09 23:39:57 +0900

    フォームを実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'items#index' 
   
   resources :items do
+    resources :orders, only: [:create, :index]
   end
   
   #resources :users  only: [:new, :create]
