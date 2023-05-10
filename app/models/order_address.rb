bb5cfd7d4db6d6b9450a1e87801f8df84fd28bfe
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-05-10 12:22:07 +0900

    test code 作成

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,11 +6,11 @@ class OrderAddress
   validates :user_id
   validates :item_id
   validates :token
-  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'は半角「ハイフン表示」で入力してください'}
   validates :shipping_prefecture_id, numericality: { other_than: 0 }
   validates :city
   validates :street  
-  validates :phone_number, format: {with: /\A[0-9]{11}\z/}
+  validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: 'は半角「ハイフンなし」で入力してください'}
 
   end
  
